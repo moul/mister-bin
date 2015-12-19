6182f5a44be6b067ab8145740145c5f539f0e61c
Manfred Touron <m@42.am>
2015-12-19 15:56:08 +0100

    Moved to structure

--- mister-bin.go
+++ mister-bin.go
@@ -24,59 +24,48 @@ func NewBinary(name string) Binary {
 	}
 }
 
+func (b *Binary) FileName() string {
+	return strings.Replace(b.Name, "/", "_", -1)
+}
+
+func (b *Binary) FilePath() string {
+	return fmt.Sprintf("/tmp/mb-%s", b.FileName())
+}
+
 func (b *Binary) Setup() error {
-	mmapAsset(b.Name)
-	return nil
-}
-
-func (b *Binary) Execute() error {
-	return nil
-}
-
-func main() {
-	for _, name := range AssetNames() {
-		bin := NewBinary(name)
-		bin.Setup()
-		bin.Execute()
-	}
-}
-
-func mmapAsset(assetName string) {
-	logrus.Infof("Exporting asset: %s", assetName)
-	asset, err := Asset(assetName)
+	logrus.Infof("Exporting asset: %s", b.Name)
+	asset, err := Asset(b.Name)
 	if err != nil {
-		logrus.Fatalf("Failed to load the asset %q: %v", assetName, err)
+		return fmt.Errorf("failed to load the asset %q: %v", b.Name, err)
 	}
 
 	length := len(asset)
 	size := int(unsafe.Sizeof(0)) * length
 	if size > mapMaxSize*int(unsafe.Sizeof(0)) {
-		logrus.Fatalf("File too big for current map size: %d > %d", size, mapMaxSize*int(unsafe.Sizeof(0)))
+		return fmt.Errorf("file too big for current map size: %d > %d", size, mapMaxSize*int(unsafe.Sizeof(0)))
 	}
 
-	filename := strings.Replace(assetName, "/", "_", -1)
-	filepath := fmt.Sprintf("/tmp/mb-%s", filename)
-	logrus.Infof("Creating map file: %q", filepath)
-	mapFile, err := os.Create(filepath)
+	logrus.Infof("Creating map file: %q", b.FilePath())
+	mapFile, err := os.Create(b.FilePath())
 	if err != nil {
-		logrus.Fatalf("Failed to create map file: %v", err)
+		return fmt.Errorf("failed to create map file: %v", err)
 	}
 
 	logrus.Infof("Seeking file")
 	if _, err = mapFile.Seek(int64(length-1), 0); err != nil {
-		logrus.Fatalf("Failed to seek: %v", err)
+		return fmt.Errorf("failed to seek: %v", err)
 	}
 
 	logrus.Infof("Writing to file")
 	if _, err = mapFile.Write([]byte(" ")); err != nil {
-		logrus.Fatalf("Failed to write to file: %v", err)
+		return fmt.Errorf("failed to write to file: %v", err)
 	}
 
 	logrus.Infof("MMAPing")
 	fd := int(mapFile.Fd())
 	mmap, err := syscall.Mmap(fd, 0, size, syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED)
 	if err != nil {
-		logrus.Fatalf("Failed to mmap: %v", err)
+		return fmt.Errorf("failed to mmap: %v", err)
 	}
 
 	logrus.Infof("Filling array var")
@@ -88,24 +77,41 @@ func mmapAsset(assetName string) {
 
 	logrus.Infof("MUNMAPing")
 	if err = syscall.Munmap(mmap); err != nil {
-		logrus.Fatalf("Failed to munmap: %v", err)
+		return fmt.Errorf("failed to munmap: %v", err)
 	}
 
 	logrus.Infof("Closing")
 	if err = mapFile.Close(); err != nil {
-		logrus.Fatalf("Failed to close: %v", err)
+		return fmt.Errorf("failed to close: %v", err)
 	}
 
 	logrus.Infof("Chmoding binary")
-	if err = os.Chmod(filepath, 0777); err != nil {
-		logrus.Fatalf("Failed to chmod program: %v", err)
+	if err = os.Chmod(b.FilePath(), 0777); err != nil {
+		return fmt.Errorf("failed to chmod program: %v", err)
 	}
 
+	return nil
+}
+
+func (b *Binary) Execute() error {
 	logrus.Infof("Executing binary")
-	cmd := exec.Command(filepath)
+	cmd := exec.Command(b.FilePath())
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		logrus.Fatalf("Failed to execute program: %v", err)
+		return fmt.Errorf("failed to execute program: %v", err)
 	}
 	logrus.Infof("Output: %s", output)
+	return nil
+}
+
+func main() {
+	for _, name := range AssetNames() {
+		bin := NewBinary(name)
+		if err := bin.Setup(); err != nil {
+			logrus.Fatalf("Failed to setup binary: %v", err)
+		}
+		if err := bin.Execute(); err != nil {
+			logrus.Fatalf("Failed to execute binary: %v", err)
+		}
+	}
 }
